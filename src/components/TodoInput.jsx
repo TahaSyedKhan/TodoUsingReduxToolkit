39abbcd1afc49338bc342159872d0388d4107aeb
TahaSyedKhan <tahasyedkhan123@gmail.com>
2024-09-04 11:49:47 +0530

    created todo app using redux toolkit

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,23 @@
+import { useDispatch } from "react-redux";
+import { addTodo } from "../redux/todoSlice";
+import { useState } from "react";
+
+function TodoInput() {
+
+    const dispatch = useDispatch(); // this dispatch function is already connected to the store
+
+    const [todo, setTodo] = useState('');
+
+    function addTodoToStore() {
+        dispatch(addTodo(todo));
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={e => setTodo(e.target.value)} value={todo}/>
+            <button onClick={addTodoToStore}>Add Todo ...</button>
+        </div>
+    )
+}
+
+export default TodoInput;
